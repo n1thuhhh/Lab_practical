7ecafce3519c13429265732eec85a37ea3c34dc0
Nitesh Gupta <niteshanujgupta2006@gmail.com>
2026-02-03 11:47:53 +0530

    Add files via upload

---
+++ tup.py
@@ -0,0 +1,22 @@
+# 1st
+# tup=(12,5,48,75,68)
+# list=list(tup)
+# list[1]=78
+# tup=tuple(list)
+# print(tup)
+
+
+# 2nd
+# a=(12,5,7,89,5)
+# a=(12,) + a[1:]
+# print(a[1:])
+
+# 3rd
+# a=[2,24,10,12,13,3,4,34,9,8]
+# print(a[0:])
+# print(a[1:9:2])
+# print(a[-3])
+
+# palindrome
+# a="NON"
+# print(a==a[-1:0:-1])
